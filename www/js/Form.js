ccb7059ca45f81c3bd5f6c68b75ec0ef2d9f9db8
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-10-03 12:29:17 +0200

    Input fields and save button created by App's createEl

--- www/js/Form.js
+++ www/js/Form.js
@@ -5,7 +5,7 @@ class Form extends App {
     this.inputFields = [
       { label: 'Namn', type: 'text', id: 'name' },
       { label: 'Epost', type: 'text', id: 'email' },
-      { label: 'Telefon', type: 'text', id: 'Telephone' },
+      { label: 'Telefon', type: 'text', id: 'telephone' },
     ]
     this.createSection()
   }
@@ -16,6 +16,15 @@ class Form extends App {
     const formHeader = this.createEl('div', formSection, { 'class': 'form-header' })
     this.createEl('i', formHeader, { 'class': 'fas fa-user-plus' })
     this.createEl('h3', formHeader).innerText = 'Lägg till kontakt'
+
+    this.inputFields.forEach(({ label, type, id }) => {
+      const inputSection = this.createEl('div', formSection, { 'class': 'input-section' })
+      this.createEl('label', inputSection, { 'for': id }).innerText = label
+      this.createEl('input', inputSection, { type, id })
+    })
+
+    const inputSection = this.createEl('div', formSection, { 'class': 'input-section' })
+    this.createEl('button', inputSection, { 'class': 'save-button' }).innerHTML = '<i class="far fa-save"></i> Spara'
   }
 
 }
