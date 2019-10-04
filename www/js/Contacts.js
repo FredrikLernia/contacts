2ac97508f1f5434f2e768e0e9e2ea6018ee283c5
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-10-04 22:53:35 +0200

    Styling adjustments

--- www/js/Contacts.js
+++ www/js/Contacts.js
@@ -33,7 +33,7 @@ class Contacts extends App {
         this.createEl('td', tr).innerText = email.join('\n')
         this.createEl('td', tr).innerText = telephone.join('\n')
         const editTd = this.createEl('td', tr, { 'class': 'edit-column' })
-        this.createEl('i', editTd, { 'class': 'far fa-edit update-contact', 'id': id })
+        this.createEl('i', editTd, { 'class': 'far fa-edit edit-contact', 'id': id })
         this.createEl('i', editTd, { 'class': 'fas fa-user-times delete-contact', 'id': id })
       })
     }
