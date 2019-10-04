42ab2c880061c91bfe70e6b3ab631f88afbc762b
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-10-04 16:40:09 +0200

    New class for showing info about a specific contact

--- www/js/Contact.js
+++ www/js/Contact.js
@@ -3,24 +3,41 @@ class Contact extends App {
   constructor(id) {
     super()
     this.id = id
+    this.tableHeadings = ['Namn', 'E-post', 'Telefon', 'Skapad', '']
     this.createSection()
   }
 
   getContact() {
     const contacts = this.loadContacts()
-    console.log(contacts)
-    const contact = contacts.find(contact => contact.id === this.id)
+    const contact = contacts.find(contact => contact.id === +this.id)
+    console.log(contact)
     return contact
   }
 
   createSection() {
     const contact = this.getContact()
-    
     const contactSection = this.createEl('div', 'div.contact', { 'class': 'contact-section' })
 
     const contactHeader = this.createEl('div', contactSection, { 'class': 'contact-header' })
     this.createEl('i', contactHeader, { 'class': 'far fa-address-card' })
-    this.createEl('h3', contactHeader).innerText = contact.name
+    this.createEl('h3', contactHeader).innerText = contact.versions[contact.chosenVersion].name
+
+    const table = this.createEl('table', contactSection)
+
+    const thead = this.createEl('thead', table)
+    const tr = this.createEl('tr', thead)
+    this.tableHeadings.forEach(tableHeading => this.createEl('th', tr).innerText = tableHeading)
+
+    const tbody = this.createEl('tbody', table)
+    contact.versions.forEach((versions, i) => {
+      const { name, email, telephone, added } = versions
+      const tr = this.createEl('tr', tbody, { 'class': contact.chosenVersion === i ? 'selected-version' : ''})
+      this.createEl('td', tr).innerText = name
+      this.createEl('td', tr).innerText = email.join('\n')
+      this.createEl('td', tr).innerText = telephone.join('\n')
+      this.createEl('td', tr).innerText = new Date(added).toLocaleDateString('sv-SE', {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'})
+      this.createEl('td', tr)
+    })
   }
 
 }
@@ -28,6 +45,26 @@ class Contact extends App {
 // Template of localStorage structure
 
 /* [
+  {
+    chosenVersion: 2,
+    versions: [
+      {
+        name: 'Kalle',
+        email: ['kalle@email.com'],
+        telephone: ['0712345612']
+      },
+      {
+        name: 'Calle',
+        email: ['calle2@hotmail.com', 'calle@gmail.com'],
+        telephone: ['0712345612']
+      },
+      {
+        name: 'Calle',
+        email: ['calle2@hotmail.com', 'calle@gmail.com'],
+        telephone: ['0754899334', '0799654471']
+      }
+    ]
+  },
   {
     chosenVersion: 1,
     versions: [
