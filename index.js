64da91d0ecc90031ef206fb5fbaf78862f697c27
Fredrik Sunding <fredrik.sunding@gmail.com>
2019-10-08 10:53:33 +0200

    Get-method in index.js to serve index.html on all routes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.use(express.static('www'))
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, './www/index.html')))
 
 app.listen(3000, () => console.log('Listening on port 3000'))
